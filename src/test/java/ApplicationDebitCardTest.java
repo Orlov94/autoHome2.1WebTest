345274f5f98e948388387eadb8fba36fa999b1df
Vladislav <vladislav_orlov_94@mail.ru>
2022-07-25 01:26:42 +0300

    add auto test4

--- src/test/java/ApplicationDebitCardTest.java
+++ src/test/java/ApplicationDebitCardTest.java
@@ -1,3 +1,4 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,7 +16,8 @@ public class ApplicationDebitCardTest {
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "/Users/vladislavorlov/Documents/Java Netology/auto home/autoHome2.1WebTest/driver/chromedriver");
+        WebDriverManager.chromedriver().setup();
+//        System.setProperty("webdriver.chrome.driver", "/Users/vladislavorlov/Documents/Java Netology/auto home/autoHome2.1WebTest/driver/chromedriver");
 
     }
 
